9441ac2a25f26a30752d918279bb6b05e3ffcc2b
liltluna <yrden@qq.com>
2024-05-12 12:35:05 +0800

    finish the calculation of auc

--- models/config.py
+++ models/config.py
@@ -9,12 +9,45 @@ from models.models import *
 from torch.utils.data import DataLoader
 from pathlib import Path
 from tqdm import tqdm
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import LabelBinarizer
 
+
+# config
+CONFIG = {
+    "step_len": 7,
+    "batch_size": 512,
+    "num_epochs": 200,
+    "seq_len": 225,
+    "lr": 10**-3,
+    "d_model": 6,
+    "h_num": 2,
+    "num_classes": 3,
+    "data_folder": 'dataset',
+    "result_folder": 'result',
+    "experiment_name": "runs/tmodel",
+    "input_w": 15,
+    "input_h": 15,
+    "label_method": "graph_label",
+    "ts_code": "000700.SZ",
+    "model_name": "CNN",
+}
+
+# Define the device
+DEVICE = "cuda:0" if torch.cuda.is_available() else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
+# device = "cpu"
+print("Using device:", DEVICE)
+if (DEVICE == 'cuda:0'):
+    print(f"Device name: {torch.cuda.get_device_name(DEVICE.index)}")
+    print(
+        f"Device memory: {torch.cuda.get_device_properties(DEVICE.index).total_memory / 1024 ** 3} GB")
+else:
+    print("NOTE: If you have a GPU, consider using it for training.")
+DEVICE = torch.device(DEVICE)
 
 
 # get dataloader
-
-
 def get_ds(config):
     train_path = f"./{config['data_folder']}/{config['label_method']}/{config['ts_code']}-train.csv"
     test_path = f"./{config['data_folder']}/{config['label_method']}/{config['ts_code']}-test.csv"
@@ -34,33 +67,55 @@ def get_ds(config):
     return train_dataloader, val_dataloader
 
 
-def run_validation(model, validation_ds, config, device, print_msg, global_step, writer, epoch, num_examples=1):
+def run_validation(model, validation_ds, config, device, print_msg, epoch):
+    model.eval()
+    output_count = 0
+    match_count = 0
+    
+    # define indicators to save
     fieldnames = ['Date', 'TARGET', 'PREDICTED']
 
-    result_dir = Path(f"./{config['result_folder']}/{config['ts_code']}/{config['model_name']}")
+    # define the file path
+    result_dir = Path(
+        f"./{config['result_folder']}/{config['ts_code']}/{config['model_name']}")
     result_dir.mkdir(parents=True, exist_ok=True)
     epoch_path = result_dir/f'epoch/epoch-{epoch}.csv'
+
     with open(epoch_path, 'w', newline='', encoding='utf-8') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-    model.eval()
-    output_count = 0
-    match_count = 0
+    # 初始化AUC列表，用于存储整个验证集上每个样本属于每个类别的预测概率
+    all_probs = [[], [], []]
+    true_labels = []
+    # Initialize a list to store AUC scores for each class
+    aucs_per_class = []
 
     with torch.no_grad():
         for batch in validation_ds:
+            target = int(batch['label'][0].item())
+            date = batch['date'][0]
             output_count += 1
             nn_input = batch["nn_input"].to(device)  # (b, seq_len)
             # check that the batch size is 1
             assert nn_input.size(0) == 1, "Batch size must be 1 for validation"
             # Run the tensors through the encoder, decoder and the projection layer
             nn_output = model.forward(nn_input)
+
+            # 确保nn_output是概率形式，如果是logits，则使用softmax转换
+            probabilities = torch.softmax(
+                nn_output, dim=1).squeeze().cpu().numpy()
+
+            # count the matched result
             _, predicted_labels = torch.max(nn_output, dim=1)
-            target = batch['label'][0]
-            date = batch['date'][0]
             if target == predicted_labels:
                 match_count += 1
+
+            # 收集每个样本的预测概率
+            for i, prob in enumerate(probabilities):
+                all_probs[i].append(prob)  # 只保留对应类别的概率
+            true_labels.append(target)
+            # out put the sample
             if output_count < 3:
                 # Print the source, target and model output
                 print_msg('-'*30)
@@ -77,21 +132,35 @@ def run_validation(model, validation_ds, config, device, print_msg, global_step,
                 writer.writerow({'Date': date, 'TARGET': int(
                     target), 'PREDICTED': int(predicted_labels)})
 
-            # accuracy_metric.update(preds=proj_output, target=target_output)
-            # if count == num_examples:
-            #     print_msg('-'*console_width)
-            #     break
-        # acc = accuracy_metric.compute().item()
         accuracy = match_count / len(validation_ds)
         print(f'accuracy: {accuracy}')
 
-    return accuracy
+    # calculate the auc
+    lb = LabelBinarizer()
+    lb.fit(range(max(true_labels) + 1))  # Fit on the range of y_true
+    y_true_bin = lb.transform(true_labels)
+    for i in range(lb.classes_.size):
+        # Extract true labels and predicted probabilities for the current class vs rest
+        y_true_class = y_true_bin[:, i]
+        y_pred_class_proba = all_probs[i]
+        
+        # Compute ROC AUC for the current class
+        auc = roc_auc_score(y_true_class, y_pred_class_proba)
+        aucs_per_class.append(auc)
+
+    return {"accuracy": accuracy,
+            "class0": aucs_per_class[0],
+            "class1": aucs_per_class[1],
+            "class2": aucs_per_class[2]}
 
 
 def train(model, device, config):
-    result_dir = Path(f"./{config['result_folder']}/{config['ts_code']}/{config['model_name']}")
+    result_dir = Path(
+        f"./{config['result_folder']}/{config['ts_code']}/{config['model_name']}")
     epoch_dir = result_dir/'epoch'
     epoch_dir.mkdir(parents=True, exist_ok=True)
+    
+    fieldnames = ['epoch', 'loss', 'acc', 'hold', 'sell', 'buy']
 
     # Make sure the weights folder exists
     # Path(f"./{config['model_folder']}").mkdir(parents=True, exist_ok=True)
@@ -103,20 +172,8 @@ def train(model, device, config):
     initial_epoch = 0
     global_step = 0
 
-    # if model_filename:
-    #     print(f'Preloading model {model_filename}')
-    #     state = torch.load(model_filename)
-    #     model.load_state_dict(state['model_state_dict'])
-    #     initial_epoch = state['epoch'] + 1
-    #     optimizer.load_state_dict(state['optimizer_state_dict'])
-    #     global_step = state['global_step']
-    # else:
-
-    print('No model to preload, starting from scratch')
-
     loss_fn = nn.CrossEntropyLoss().to(device)
 
-    fieldnames = ['epoch', 'loss', 'acc']
     with open(result_dir/"statistic.csv", 'w', newline='', encoding='utf-8') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -142,10 +199,6 @@ def train(model, device, config):
             loss = loss_fn(nn_output, label)
             batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
 
-            # Log the loss
-            # writer.add_scalar('train loss', loss.item(), global_step)
-            # writer.flush()
-
             # Backpropagate the loss
             loss.backward()
 
@@ -156,19 +209,21 @@ def train(model, device, config):
             global_step += 1
 
         # Run validation at the end of every epoch
-        accuracy = run_validation(model=model, validation_ds=val_dataloader, global_step=global_step, device=device,print_msg= lambda msg: batch_iterator.write(
-            msg), writer=None, epoch=epoch, config=config)
-        
+        val_result = run_validation(model=model, validation_ds=val_dataloader, device=device,
+                                    print_msg=lambda msg: batch_iterator.write(msg), epoch=epoch, config=config)
+
         with open(result_dir/"statistic.csv", 'a', newline='', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             # 将数据写入
             writer.writerow(
-                {'epoch': epoch, 'loss': loss.item(), 'acc': accuracy})
+                {'epoch': epoch, 'loss': loss.item(), 'acc': val_result['accuracy'], 'hold': val_result['class0'], 'sell': val_result['class1'], 'buy': val_result['class2']})
 
-def plot_result(config):
+
+def plot_loss_result(config):
 
     window = 10
-    statistic_dir = Path(f"./{config['result_folder']}/{config['ts_code']}/{config['model_name']}")
+    statistic_dir = Path(
+        f"./{config['result_folder']}/{config['ts_code']}/{config['model_name']}")
     statistic_path = statistic_dir/"statistic.csv"
     # window = 10
     # 步骤1: 加载CSV文件中的数据
@@ -178,7 +233,11 @@ def plot_result(config):
     data['Smoothed Loss'] = data['loss'].rolling(window=window).mean()
     data['Smoothed Accuracy'] = data['acc'].rolling(window=window).mean()  # 假设'accuracy'是准确率的列名
 
+    # 对每个AUC相关的列计算移动平均值
+    for auc_col in ['hold', 'sell', 'buy']:
+        data[f'Smoothed {auc_col}'] = data[auc_col].rolling(window=window).mean()
     # 提取epoch、平滑后的loss和accuracy数据
+
     epochs = data['epoch']
     losses = data['Smoothed Loss']
     accuracies = data['Smoothed Accuracy']
@@ -190,7 +249,11 @@ def plot_result(config):
     plt.plot(epochs, losses, label='Loss', color='blue')  # 绘制平滑后的loss曲线，颜色设为蓝色
 
     # 绘制accuracy曲线
-    plt.plot(epochs, accuracies, label='Accuracy', color='green')  # 注意原注释中的颜色被修正为绿色
+    plt.plot(epochs, accuracies, label='Accuracy',color='red') 
+    # 绘制新的AUC曲线
+    plt.plot(epochs, data['Smoothed hold'], label='Hold AUC', color='orange')
+    plt.plot(epochs, data['Smoothed sell'], label='Sell AUC', color='purple')
+    plt.plot(epochs, data['Smoothed buy'], label='Buy AUC', color='brown')
 
     # 添加标题和坐标轴标签
     plt.title(f"{config['model_name']} On {config['ts_code']}")
@@ -204,5 +267,40 @@ def plot_result(config):
     plt.grid(False)  # 原本为False，这里假设你想显示网格，根据需要可以调整
 
     # 直接保存图像，而不是显示
-    plt.savefig(statistic_dir/f"{config['model_name']}_On_{config['ts_code']}.png")
-    
+    plt.savefig(statistic_dir /
+                f"{config['model_name']}_On_{config['ts_code']}.png")
+
+
+def plot_finicial_evalutaion(config):
+    pass
+
+
+def print_confusion_matrix(config):
+    result_dir = Path(
+        f"./{config['result_folder']}/{config['ts_code']}/{config['model_name']}")
+    epoch_dir = result_dir/'epoch'
+    # define the file name list
+    file_names = [f"epoch-{i}.csv" for i in range(100, 200)]
+
+    # 初始化一个空的DataFrame用于存储所有数据
+    all_data = pd.DataFrame()
+
+    # 循环读取每个CSV文件并合并
+    for file_name in file_names:
+        data = pd.read_csv(epoch_dir/file_name)
+        # 忽略'Date'列
+        data = data.drop(columns=['Date'])
+        all_data = pd.concat([all_data, data], ignore_index=True)
+
+    # 计算混淆矩阵
+    targets = all_data['TARGET']
+    predicted = all_data['PREDICTED']
+
+    # 确保TARGET和PREDICTED列只包含0, 1, 2这三个值
+    targets = targets.astype(int)
+    predicted = predicted.astype(int)
+
+    cm = confusion_matrix(targets, predicted, labels=[0, 1, 2])
+
+    print("混淆矩阵:")
+    print(cm)
--- phase2.py
+++ phase2.py
@@ -2,41 +2,7 @@ from models.dataset import *
 from models.models import *
 from models.config import *
 
-# config
-CONFIG = {
-    "step_len": 7,
-    "batch_size": 512,
-    "num_epochs": 200,
-    "seq_len": 225,
-    "lr": 10**-3,
-    "d_model": 6,
-    "h_num": 2,
-    "num_classes": 3,
-    "data_folder": 'dataset',
-    "result_folder": 'result',
-    "experiment_name": "runs/tmodel",
-    "input_w": 15,
-    "input_h": 15,
-    "label_method": "graph_label",
-    "ts_code": "000700.SZ",
-    "model_name": "CNN",
-}
-
-# Define the device
-device = "cuda:0" if torch.cuda.is_available() else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
-# device = "cpu"
-print("Using device:", device)
-if (device == 'cuda:0'):
-    print(f"Device name: {torch.cuda.get_device_name(device.index)}")
-    print(
-        f"Device memory: {torch.cuda.get_device_properties(device.index).total_memory / 1024 ** 3} GB")
-else:
-    print("NOTE: If you have a GPU, consider using it for training.")
-device = torch.device(device)
-
-
-# train 
-model = GraphCNN(input_h=CONFIG['input_h'], input_w=CONFIG['input_w'], num_classes=CONFIG['num_classes']).to(device)
-
-train(model=model, config=CONFIG, device=device)
-plot_result(config=CONFIG)
+# train and plot the result of CNN 
+model = GraphCNN(input_h=CONFIG['input_h'], input_w=CONFIG['input_w'], num_classes=CONFIG['num_classes']).to(DEVICE)
+train(model=model, config=CONFIG, device=DEVICE)
+plot_loss_result(config=CONFIG)
